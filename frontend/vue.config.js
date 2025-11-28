71562716a17e66d7ea7e44cd43a0992fa31436eb
Nick Dieda <nickeagle888@gmail.com>
2025-11-28 18:11:01 +0300

    first commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target:"https://crm-backend.onrender.com",
+        //  process.env.VUE_APP_API_URL || 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
+})
+
+
